687d7d606cdbeaa71bc1cd8edc9368b3314a5b25
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-03-21 06:01:54 -0700

    feat: add Dockerfile and update build script for improved containerization

--- BUILD.sh
+++ BUILD.sh
@@ -1,3 +1,13 @@
-git pull;
-trunk build --release;
-sudo docker run -d -v /home/whitepi/mtv-movs-leptos/dist:/usr/share/nginx/html:ro -p 9091:80 nginx:bookworm;
+# git pull;
+# trunk build --release;
+# sudo docker run -d -v /home/whitepi/mtv-movs-leptos/dist:/usr/share/nginx/html:ro -p 9091:80 nginx:bookworm;
+
+
+# Navigate to the directory containing the Dockerfile
+cd /home/pipi/mtv-movs-leptos
+
+# Build the Docker image
+docker build -t mtvmovslep:latest .
+
+# Run the Docker container
+docker run -d -p 9091:80 mtvmovslep:latest
