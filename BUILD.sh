f223f08dfdd29654d3068397409e90a8744317ca
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-03-21 04:59:59 -0700

    fix: update port in docker run command for consistency

--- BUILD.sh
+++ BUILD.sh
@@ -1,3 +1,3 @@
 git pull;
 trunk build --release;
-sudo docker run -d --name mtvmovslep -v /home/whitepi/mtv-movs-leptos/dist:/usr/share/nginx/html:ro -p 9000:80 nginx:bookworm;
+sudo docker run -d -v /home/whitepi/mtv-movs-leptos/dist:/usr/share/nginx/html:ro -p 9091:80 nginx:bookworm;
