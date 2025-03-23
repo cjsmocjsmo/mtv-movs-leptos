8ccc0cfeef1f906e3f34e8db3263ad12a1a8668c
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-03-23 09:56:12 -0700

    fix: add multi-architecture Dockerfiles and update BUILD.sh for architecture-specific builds

--- BUILD.sh
+++ BUILD.sh
@@ -2,15 +2,22 @@
 cd /home/whitepi/mtv-tv-leptos/
 
 git pull;
+
 trunk build --release;
 
 arch=$(uname -m)
+
 if [ "$arch" = "aarch64" ]; then
-    docker build -t mtvmovslep:arm64 .
+    docker build -t mtvmovslep:arm64 -f ./arm64/Dockerfile .
     docker run -d -p 9092:80 mtvmovslep:arm64
 fi
 
 if [ "$arch" = "arm32v7" ]; then
-    docker build -t mtvmovslep:arm32 .
+    docker build -t mtvmovslep:arm32 -f ./arm32/Dockerfile .
     docker run -d -p 9092:80 mtvmovslep:arm32
+fi
+
+if [ "$arch" = "x86_64" ]; then
+    docker build -t mtvmovslep:amd64 -f ./amd64/Dockerfile .
+    docker run -d -p 9092:80 mtvmovslep:amd64
 fi
